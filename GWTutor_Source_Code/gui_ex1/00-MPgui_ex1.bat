2d03a4bd8c70c551d3e2f7f6db3d5d17ecd6305c
andrewtbanks <40248294+andrewtbanks@users.noreply.github.com>
2019-04-09 16:33:43 -0500

    Delete 00-MPgui_ex1.bat

--- GWTutor_Source_Code/gui_ex1/00-MPgui_ex1.bat
+++
@@ -1,5 +0,0 @@
-del C:\Users\a561b615\Desktop\Gwtutor\GWTutor_Source_Code\gui_ex1\gui_ex1_modpathID.txt
-cd /d C:\Users\a561b615\Desktop\Gwtutor\GWTutor_Source_Code\gui_ex1
-C:\Users\a561b615\Desktop\Gwtutor\GWTutor_Source_Code\Mpath7.exe ..\gui_ex1\gui_ex1
-break>"C:\Users\a561b615\Desktop\Gwtutor\GWTutor_Source_Code\gui_ex1\gui_ex1_modpathID.txt
-exit
